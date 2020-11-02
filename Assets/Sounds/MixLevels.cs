d8ded843cc504d3cd3ba9af5aab4151a6c7cd190
조희석 <dswill038@gmail.com>
2020-11-03 00:08:21 +0900

    피격 이펙트 v0.01 Bgm, 총 소리 추가 일시정지 상태의 오디오 믹서 스냅샷 추가

---
+++ Assets/Sounds/MixLevels.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MixLevels : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+    public Text pausedText;
+    public AudioMixerSnapshot paused;
+    public AudioMixerSnapshot unpaused;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pausedText.enabled = !pausedText.enabled;
+            Pause();
+        }    
+    }
+
+    public void Pause()
+    {
+        // 일시정지
+        Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+        Lowpass();
+    }
+
+    public void Lowpass()
+    {
+        if (Time.timeScale == 0)
+        {
+            paused.TransitionTo(0.01f);
+        }
+        else
+        {
+            unpaused.TransitionTo(0.01f);
+        }
+    }
+    
+    public void SetSfxLvl(float sfxLvl)
+    {
+        masterMixer.SetFloat("sfxVol", sfxLvl);
+    }
+
+    public void SetBgmLvl(float bgmLvl)
+    {
+        masterMixer.SetFloat("bgmVol", bgmLvl);
+    }
+
+    public void ClearVolume()
+    {
+        masterMixer.ClearFloat("bgmVol");
+    }
+}
