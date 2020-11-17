abe568294dc23c0d090998cc0c64bcff87b5c1ef
sochangho <ckdgh940@naver.com>
2020-11-17 11:29:45 +0900

    MuzzleEffect 생성

---
+++ Assets/changhoScript/Manager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem muzzleEffect_prefab;
+    private ParticleSystem muzzleEffect;
+    [SerializeField] private GameObject paticlePos;
+
+    public static Manager instance = null;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+
+            instance = this;
+        }
+        else if(instance != null)
+        {
+            Destroy(this.gameObject);
+
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        muzzleEffect =Instantiate(muzzleEffect_prefab, paticlePos.transform.position, paticlePos.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void MuzzlePaticleOn()
+    {
+        muzzleEffect.transform.position = paticlePos.transform.position;
+        muzzleEffect.transform.rotation = paticlePos.transform.rotation;
+
+        muzzleEffect.gameObject.SetActive(true);
+    }
+
+}
---
+++ Assets/changhoScript/MuzzleEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuzzleEffect : MonoBehaviour
+{
+    private float MuzzleEffectLife;
+
+    private void Update()
+    {
+        MuzzleEffectLife -= Time.deltaTime;
+
+        if(MuzzleEffectLife < 0)
+        {
+            MuzzleEffectLife = 0.3f;
+            this.gameObject.SetActive(false);
+
+        }
+
+
+    }
+
+    private void OnEnable()
+    {
+        MuzzleEffectLife = 0.3f;
+        
+    }
+
+
+
+}
