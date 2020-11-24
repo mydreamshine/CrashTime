226f523b4a0d8851af70dd51a3573d04a011077e
sochangho <ckdgh940@naver.com>
2020-11-24 13:54:58 +0900

    총알 발사 / 궤적

---
+++ Assets/changho/changhoScript/BulletObjectPool.cs
@@ -0,0 +1,87 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace changhoScript
+{
+
+    public class BulletObjectPool : MonoBehaviour
+    {
+        
+        public GameObject prefab;
+        public int initialSize;
+
+        private readonly Stack<GameObject> instances = new Stack<GameObject>();
+
+        private void Start()
+        {
+            for (var i = 0; i < initialSize; i++)
+            {
+                var obj = CreateInstance(); 
+                obj.SetActive(false); 
+                instances.Push(obj); 
+            }
+        }
+
+       
+        public GameObject GetObject()  
+        {
+            var obj = instances.Count > 0 ? instances.Pop() : CreateInstance();
+
+            
+
+            obj.SetActive(true);
+            obj.transform.position = transform.position;
+            obj.transform.rotation = transform.rotation;
+            return obj;
+        }
+
+       
+        public void ReturnObject(GameObject obj) 
+        {
+            var pooledObject = obj.GetComponent<PooledObject>();
+            
+            
+
+            obj.SetActive(false);
+            if (!instances.Contains(obj)) 
+            {
+                instances.Push(obj); 
+            }
+        }
+
+        public void Reset() 
+        { 
+            var objectsToReturn = new List<GameObject>();
+            foreach (var instance in transform.GetComponentsInChildren<PooledObject>())
+            { 
+                if (instance.gameObject.activeSelf) 
+                {
+                    objectsToReturn.Add(instance.gameObject);  
+                }
+            }
+            foreach (var instance in objectsToReturn)   
+            {
+                ReturnObject(instance); 
+            }
+        }
+
+
+        private GameObject CreateInstance() 
+        {
+            var obj = Instantiate(prefab ,transform.position,transform.rotation);   
+            var pooledObject = obj.AddComponent<PooledObject>();
+            pooledObject.pool = this;
+           
+            obj.transform.SetParent(transform);    
+            return obj;
+        }
+    }
+
+    public class PooledObject : MonoBehaviour
+    {
+        public BulletObjectPool pool;
+    }
+
+
+}
---
+++ Assets/changho/changhoScript/MuzzleControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using changhoScript;
+public class MuzzleControl : MonoBehaviour
+{
+    [SerializeField]
+    private BulletObjectPool bulletPool;
+
+    [SerializeField]
+    private int BulletSpeed;
+
+    public GameObject bulletPos;
+
+    bool check = true;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0)&&check)
+        {
+            check = false;
+            Manager.instance.MuzzlePaticleOn();
+            var clone = bulletPool.GetObject();
+            clone.transform.Rotate(90, 0, 0);
+            clone.GetComponent<Rigidbody>().AddForce(transform.forward * BulletSpeed);
+
+            StartCoroutine(Wait());
+        }
+    }
+
+
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(1f);
+        check = true;
+
+    }
+
+}
